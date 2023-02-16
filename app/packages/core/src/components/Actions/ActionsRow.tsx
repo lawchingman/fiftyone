bd1e825af9b7d404306105bdbed3f3f58be55e3f
Lanny W <lanzhenwang9@gmail.com>
2023-02-16 12:15:54 -0600

    break up the main component

--- app/packages/core/src/components/Actions/ActionsRow.tsx
+++ app/packages/core/src/components/Actions/ActionsRow.tsx
@@ -35,8 +35,8 @@ import { PillButton } from "../utils";
 import OptionsActions from "./Options";
 import Patcher, { patchesFields } from "./Patcher";
 import Selector from "./Selected";
-import Similar, { isImageSimilaritySearch } from "./Similar";
 import Tagger from "./Tagger";
+import Similar, { isImageSimilaritySearch } from "./similar/Similar";
 
 export const shouldToggleBookMarkIconOnSelector = selector<boolean>({
   key: "shouldToggleBookMarkIconOn",
